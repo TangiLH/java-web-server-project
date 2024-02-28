3fe3f988b9f3d0dd5273352452774faea5a5912c
Tangi Le Henaff <tangi_lehenaff@ens.univ-artois.fr>
2024-02-28 11:24:00 +0100

    ajout du constructeur vide à la fabrique LecteurFichier

--- src/LecteurFichierFabrique.java
+++ src/LecteurFichierFabrique.java
@@ -1,6 +1,9 @@
 import java.io.FileNotFoundException;
 
 public abstract class LecteurFichierFabrique implements LecteurFichierInterface{
+    private LecteurFichierFabrique(){
+        //constructeur vide pour classe abstraite
+    }
     public static LecteurFichierInterface creerLecteur(String nomFichier){
         LecteurFichierInterface lfi;
         try{
